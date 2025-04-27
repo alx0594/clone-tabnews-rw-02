82be9616ec45d8dc5a2ad3a8f3f83ac42b192b66
Alex <alexgithub94@gmail.com>
2025-04-27 18:38:32 -0300

    adiciona consulta ao banco de dados

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,18 @@
-function status(request, response) {
-  response.status(200).json({ status: "Ok" });
+import database from "infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+
+  response.status(200).json({
+    database: {
+      dependencies: {
+        version: "16.0",
+        max_connetions: 100,
+        opened_connections: 1,
+      },
+    },
+  });
 }
 
 export default status;
