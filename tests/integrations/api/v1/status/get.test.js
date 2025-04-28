c1a34ae8100a7d2c08a305c6ccd0cbf30b7c984c
Alex <alexgithub94@gmail.com>
2025-04-28 18:57:51 -0300

    adiciona queries e testes de integração

--- tests/integrations/api/v1/status/get.test.js
+++ tests/integrations/api/v1/status/get.test.js
@@ -1,4 +1,15 @@
 test("GET to /api/v1/status should return status code 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  console.log(responseBody.database.dependencies.version);
+
+  expect(responseBody.update_at).toBeDefined();
+
+  expect(responseBody.database.dependencies.version).toEqual("16.8");
+
+  expect(responseBody.database.dependencies.max_connections).toEqual("100");
+
+  expect(responseBody.database.dependencies.opened_connections).toEqual(1);
 });
